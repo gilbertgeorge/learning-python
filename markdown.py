27437130457883e8a445ec34c6da2b8652af2abb
gilbertgeorge <george@grgilbert.com>
2022-12-22 17:03:45 -0500

    start markdown

--- markdown.py
+++ markdown.py
@@ -1,5 +1,14 @@
 def markdown():
-    print('test2')
+    md = '''
+# John Lennon
+or ***John Winston Ono Lennon*** was one of *The Beatles*.
+Here are the songs he wrote I like the most:
+* Imagine
+* Norwegian Wood
+* Come Together
+* In My Life
+* ~~Hey Jude~~ (that was *McCartney*)'''
+    print(md)
 
 
 if __name__ == '__main__':
