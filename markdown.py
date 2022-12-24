0b4151c98b8dcf874688860b4a23666ec40ebc20
GEORGEGPC\George <george@grgilbert.com>
2022-12-24 11:45:45 -0500

    more md

--- markdown.py
+++ markdown.py
@@ -1,14 +1,19 @@
 def markdown():
-    md = '''
-# John Lennon
-or ***John Winston Ono Lennon*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
-* Imagine
-* Norwegian Wood
-* Come Together
-* In My Life
-* ~~Hey Jude~~ (that was *McCartney*)'''
-    print(md)
+    available_formatters = ['plain', 'bold', 'italic', 'header', 'link', 'inline-code', 'ordered-list',
+                            'unordered-list', 'new-line']
+    special_commands = ['!help', '!done']
+    help_msg = 'Available formatters: ' \
+               'plain bold italic header link inline-code ordered-list unordered-list new-line\n' \
+               'Special commands: !help !done'
+    unknown_msg = 'Unknown formatting type or command'
+    while True:
+        command = input('Choose a formatter:')
+        if command not in available_formatters + special_commands:
+            print(unknown_msg)
+        if command == '!help':
+            print(help_msg)
+        if command == '!done':
+            break
 
 
 if __name__ == '__main__':
