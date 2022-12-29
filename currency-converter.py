06247332341eb86c3fa2bdbca1d9a78edc748cd7
gilbertgeorge <george@grgilbert.com>
2022-12-29 17:12:07 -0500

    more conversion

--- currency-converter.py
+++ currency-converter.py
@@ -1,6 +1,18 @@
-def convert():
-    print('convert!')
+def get_global_currencies(coins):
+    currency_conversions = {'RUB': 2.98, 'ARS': 0.82, 'HNL': 0.17, 'AUD': 1.9622, 'MAD': 0.208}
+    for currency in currency_conversions:
+        amount = round(currency_conversions[currency] * coins, 2)
+        print(f'I will get {amount} {currency} from the sale of {coins} conicoins.')
+
+
+def convert_coni_coins(coins, exchange_rate):
+    return coins * exchange_rate
+
+
+def currency_converter():
+    coni_coin_count = float(input())
+    get_global_currencies(coni_coin_count)
 
 
 if __name__ == '__main__':
-    convert()
+    currency_converter()
