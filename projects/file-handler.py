479e27e13d02e71abc94bbd8407c271d99c054d8
GEORGEGPC\George <george@grgilbert.com>
2023-01-02 16:34:11 -0500

    check for dupes based on file hash

--- projects/file-handler.py
+++ projects/file-handler.py
@@ -1,11 +1,13 @@
 import argparse
 import os
+import hashlib
 
 
 class FileHandler:
     def __init__(self, filename='.'):
         self.filename = filename
         self.files = self.get_files()
+        self.filtered_files = {}
 
     def __str__(self):
         return f'File:({self.filename})'
@@ -30,11 +32,34 @@ class FileHandler:
                 rev_multidict.setdefault(value, set()).add(key)
 
         sort_order = True if order == 'descending' else False
+        filtered_files = {}
         for key, value in sorted(rev_multidict.items(), reverse=sort_order):
             if len(value) > 1:
                 print(f'{key} bytes')   # print the size of the file
-                print(*value, sep='\n')  # print the file names
+                for each_file in value:
+                    print(f'{each_file}')
+                filtered_files[key] = value
                 print()
+        self.filtered_files = filtered_files
+
+    def check_duplicates(self):
+        index = 1
+        for key, value in self.filtered_files.items():
+            hash_dict = {}
+            for each_file in value:
+                file_hash = hashlib.md5(open(each_file, 'rb').read()).hexdigest()
+                hash_dict.setdefault(file_hash, set()).add(each_file)
+            print_byte = True
+            for h_key, h_value in hash_dict.items():
+                if len(h_value) > 1:
+                    if print_byte:
+                        print(f'{key} bytes')
+                        print_byte = False
+                    print(f'Hash: {h_key}')
+                    for each_dupe_file in h_value:
+                        print(f'{index}. {each_dupe_file}')
+                        index += 1
+            print()
 
 
 def get_sort_options():
@@ -62,6 +87,17 @@ def file_handler():
         sorting_dictionary = get_sort_options()
         handler.print_file_list_by_extension(sorting_dictionary['format'],
                                              sorting_dictionary['option'])
+        while True:
+            print('Check for duplicates?')
+            dupe_check = input()
+            if dupe_check == 'yes':
+                handler.check_duplicates()
+                break
+            elif dupe_check == 'no':
+                break
+            else:
+                print('Wrong option')
+
     else:
         print('Directory is not specified')
 
