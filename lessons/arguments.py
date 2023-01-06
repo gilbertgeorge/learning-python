f46a686a51d77f05a78cc645c4b8b5918b84a2d1
gilbertgeorge <george@grgilbert.com>
2023-01-06 17:22:22 -0500

    kwargs

--- lessons/arguments.py
+++ lessons/arguments.py
@@ -18,12 +18,30 @@ def average_grade(*grades):
     return round(sum(grades) / len(grades), 1)
 
 
+def capital(**kwargs):
+    for key, value in kwargs.items():
+        print(value, "is the capital city of", key)
+
+
+def say_bye(**names):
+    for name in names:
+        print("Au revoir,", name)
+        print("See you on", names[name]["next appointment"])
+        print()
+
+
 if __name__ == '__main__':
-    print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-    will_survive("Daenerys Targaryen", "Arya Stark", "Brienne of Tarth")
-    print(recipe("meat", "fish"))
-    print(recipe("meat", "fish", sep="and"))
-    print(recipe("meat", "fish", sep="&"))
-    print(*"fun")
-    print(*[5, 10, 15])
-    print(average_grade(3, 4, 5, 3))
+    # print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+    # will_survive("Daenerys Targaryen", "Arya Stark", "Brienne of Tarth")
+    # print(recipe("meat", "fish"))
+    # print(recipe("meat", "fish", sep="and"))
+    # print(recipe("meat", "fish", sep="&"))
+    # print(*"fun")
+    # print(*[5, 10, 15])
+    # print(average_grade(3, 4, 5, 3))
+
+    capital(Canada="Ottawa", Estonia="Tallinn", Venezuela="Caracas", Finland="Helsinki")
+
+    humans = {"Laura": {"next appointment": "Tuesday"},
+              "Robin": {"next appointment": "Friday"}}
+    say_bye(**humans)
