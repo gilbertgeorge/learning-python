d2ff63db457a0bf6983a20f7a16118f0797a2d7b
gilbertgeorge <george@grgilbert.com>
2023-01-10 18:10:23 -0500

    needs work, dont think eval will cut it alone

--- projects/smart_calc.py
+++ projects/smart_calc.py
@@ -1,24 +1,68 @@
 import math
 
 
-def smart_calc():
-    while True:
-        user_input = input()
-        if user_input == '/exit':
-            print('Bye!')
-            break
-        elif user_input == '/help':
-            print('The program performs addition and subtraction of numbers')
-        elif user_input == '':
-            continue
-        elif user_input[0] == '/':
-            print('Unknown command')
+class SmartCalculator:
+    def __init__(self):
+        self.variables = {}
+        self.current_expression = ''
+        self.user_prompt()
+
+    def assign_variable(self):
+        split_expression = self.current_expression.split('=')
+        variable = split_expression[0].strip()
+        value = split_expression[1].strip()
+        if any(char.isdigit() for char in variable):
+            print('Invalid identifier')
+        else:
+            if len(split_expression) > 2:
+                print('Invalid assignment')
+            elif any(char.isalpha() for char in value):
+                if any(char.isdigit() for char in value):
+                    print('Invalid assignment')
+                elif value in self.variables.keys():
+                    self.variables[variable] = value
+                else:
+                    print('Unknown variable')
+            else:
+                self.variables[variable] = int(value)
+            # print(self.variables)
+
+    def evaluate_expression(self):
+        if '=' in self.current_expression:
+            self.assign_variable()
+        elif all(char not in [' ', '+', '-', '='] for char in self.current_expression):
+            if any(char.isdigit() for char in self.current_expression):
+                print('Invalid identifier')
+            elif self.current_expression in self.variables.keys():
+                print(self.variables[self.current_expression])
+            else:
+                print('Unknown variable')
         else:
             try:
-                print(eval(user_input))
-            except (NameError, SyntaxError) as e:
-                print('Invalid expression')
+                local = self.variables.copy()
+                print(eval(self.current_expression, local))
+                del local
+            except NameError:
+                print(f'Invalid identifier')
+            except SyntaxError:
+                print(f'Invalid expression')
+
+    def user_prompt(self):
+        while True:
+            user_input = input()
+            if user_input == '/exit':
+                print('Bye!')
+                break
+            elif user_input == '/help':
+                print('The program performs math operations of numbers and can store variables')
+            elif user_input == '':
+                continue
+            elif user_input[0] == '/':
+                print('Unknown command')
+            else:
+                self.current_expression = user_input
+                self.evaluate_expression()
 
 
 if __name__ == '__main__':
-    smart_calc()
+    calc = SmartCalculator()
